2486ca5c6134de05a21eeb1285fd7f5cf8abaeba
TinaK92 <tinakonopko@gmail.com>
2024-12-26 13:17:44 -0500

    importing os

--- migrations/versions/a3d066d9d270_init.py
+++ migrations/versions/a3d066d9d270_init.py
@@ -1,16 +1,19 @@
-"""empty message
+"""init
 
-Revision ID: 82a1437ade79
+Revision ID: a3d066d9d270
 Revises: 
-Create Date: 2024-12-10 11:56:16.710326
+Create Date: 2024-12-26 13:08:24.264068
 
 """
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
-revision = '82a1437ade79'
+revision = 'a3d066d9d270'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -31,6 +34,8 @@ def upgrade():
     sa.UniqueConstraint('email'),
     sa.UniqueConstraint('username')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
     op.create_table('listings',
     sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
     sa.Column('user_id', sa.Integer(), nullable=False),
@@ -43,6 +48,8 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE listings SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
