3aabe0e64e237ba5a03d3847b8ff236c34a45f87
TinaK92 <tinakonopko@gmail.com>
2024-12-27 16:16:56 -0500

    created enviorments use for production in new migrations file

--- app/models/listing_size.py
+++ app/models/listing_size.py
@@ -3,11 +3,20 @@ from sqlalchemy.sql.sqltypes import Integer
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 
-
 listing_sizes = Table(
     "listing_sizes",
     db.Model.metadata,
-    db.Column("listing_id", db.Integer, db.ForeignKey(add_prefix_for_prod("listings.id")), primary_key=True),
-    db.Column("size_id", db.Integer, db.ForeignKey(add_prefix_for_prod("sizes.id")), primary_key=True),
+    db.Column(
+        "listing_id",
+        db.Integer,
+        db.ForeignKey(add_prefix_for_prod("listings.id")),
+        primary_key=True,
+    ),
+    db.Column(
+        "size_id",
+        db.Integer,
+        db.ForeignKey(add_prefix_for_prod("sizes.id")),
+        primary_key=True,
+    ),
     schema=SCHEMA if environment == "production" else None,
 )
