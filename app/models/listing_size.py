1daf8c9ec9c86240161366a76a57ba280f4cdac7
TinaK92 <tinakonopko@gmail.com>
2024-12-27 15:29:46 -0500

    models init.py imports were out of order so they were fixed

--- app/models/listing_size.py
+++ app/models/listing_size.py
@@ -1,9 +1,10 @@
-from .db import db, environment, SCHEMA, add_prefix_for_prod
+from app.models import db, SCHEMA, environment, add_prefix_for_prod
+from sqlalchemy.schema import Table
 
-listing_sizes = db.Table(
+listing_sizes = Table(
     "listing_sizes",
     db.Model.metadata,
     db.Column("listing_id", db.Integer, db.ForeignKey(add_prefix_for_prod("listings.id")), primary_key=True),
     db.Column("size_id", db.Integer, db.ForeignKey(add_prefix_for_prod("sizes.id")), primary_key=True),
-    schema=SCHEMA if environment == "production" else None  # Add schema for production
+    schema=SCHEMA if environment == "production" else None,
 )
