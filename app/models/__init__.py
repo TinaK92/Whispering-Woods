206268c5c20701a1c8796634e445708ae57d6de4
TinaK92 <tinakonopko@gmail.com>
2024-12-27 16:04:31 -0500

    changed order of imports in init.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,7 @@
 from .db import db, environment, SCHEMA
+from .listing_size import listing_sizes
+from .listing_color import listing_colors
 from .user import User
 from .listing import Listing
 from .size import Size
 from .color import Color
-from .listing_size import listing_sizes
-from .listing_color import listing_colors
